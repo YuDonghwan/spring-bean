9ff04d68925d86df29953e56abe6033ceaa628cb
dong <ydhwan0905@gmail.com>
2024-01-04 21:23:41 +0900

    스프링 기본편

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,29 @@
+package hello.core.member;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+
+
+    private final MemberRepository memberRepository;
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
